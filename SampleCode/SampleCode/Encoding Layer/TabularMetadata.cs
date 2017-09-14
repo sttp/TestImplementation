caf85af3b623aac3dd2368770f277257f23d12ba
Steven Chisholm <steven@chizware.com>
2017-09-14 13:52:16 -0500

    Added 2 different metadata transport constructs.

---
+++ SampleCode/SampleCode/Encoding Layer/TabularMetadata.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using sttp;
+
+namespace SampleCode.Encoding_Layer
+{
+    //------------------------
+    //
+    //  Provides a means to serialize a tabular data set.
+    //
+    //------------------------
+
+    public class TabularMetadataSchema
+    {
+        public int SchemeRuntimeID;
+        public string TableName;
+        public string[] ColumnNames;
+        public ValueTypeCode[] ColumnTypes;
+    }
+
+    public class TabularMetadataTable
+    {
+        public int TableRuntimeID;
+        public string TableName;
+        public int MetadataSchemaID;
+    }
+
+    public class TabularMetadataRecord
+    {
+        public int TableRuntimeID;
+        public int RowNumber;
+        public byte[][] Fields;
+    }
+
+}
---
+++ SampleCode/SampleCode/Encoding Layer/TreeMetadata.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using sttp;
+
+namespace SampleCode.Encoding_Layer
+{
+    //------------------------
+    //
+    //  Provides a means to serialize a relational data set. 
+    //
+    //------------------------
+
+    public class TreeMetadata
+    {
+        public int RuntimeID;
+        public int ParentObject;
+        public string AttributeName;
+        public ValueTypeCode AttributeType;
+        public byte[] AttributeValue;
+    }
+}
