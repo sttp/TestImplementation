dceabdbd19311d21087d4d2d6fb97b55dfc76af6
Steven Chisholm <steven@chizware.com>
2017-10-05 08:54:08 -0500

    Code cleanup.

--- Sttp.WireProtocol/DataPointPacket/DataPointCommand.cs
+++ Sttp.WireProtocol/DataPointPacket/DataPointCommand.cs
@@ -2,7 +2,19 @@
 {
     public enum DataPointCommand
     {
+        /// <summary>
+        /// Map a new runtime point id.
+        /// </summary>
         MapRuntimeID,
+
+        /// <summary>
+        /// Sends a data point of the normal size.
+        /// </summary>
         SendDataPoint,
+
+        /// <summary>
+        /// Sends a data point that is larger than 64bytes in size.
+        /// </summary>
+        SendLargeDataPoint,
     }
 }
