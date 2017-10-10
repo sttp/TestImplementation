25580bdfb0d843e52fb8db17cf0a383adb49ac5a
Steven Chisholm <steven@chizware.com>
2017-10-09 22:43:00 -0500

    Added commands in the NegotiateSession for changing the named instance of a connection.

--- Sttp.WireProtocol/NegotiateSessionPacket/NegotiateSessionCommandCode.cs
+++ Sttp.WireProtocol/NegotiateSessionPacket/NegotiateSessionCommandCode.cs
@@ -3,6 +3,28 @@ namespace Sttp.WireProtocol
 {
     public enum NegotiateSessionCommandCode
     {
+        /// <summary>
+        /// Changes the default instance name for subscribing from.
+        /// Payload:
+        /// string InstanceName
+        /// </summary>
+        ChangeInstance,
+
+        /// <summary>
+        /// Gets all of the named instances for this publisher.
+        /// </summary>
+        GetAllInstances,
+
+        /// <summary>
+        /// The name of each instance.
+        /// 
+        /// int NumberOfInstances
+        /// {
+        ///     String Name,
+        ///     String Description
+        /// }
+        /// </summary>
+        InstanceList,
         
     }
 }
