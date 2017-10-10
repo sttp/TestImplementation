9eb9e0f50663ed5be18d64ff29dca26c1c397681
Steven Chisholm <steven@chizware.com>
2017-10-09 22:04:01 -0500

    First draft of the sub-commands for the subscription layer.

--- Sttp.WireProtocol/SubscriptionPacket/SubscriptionCommand.cs
+++ Sttp.WireProtocol/SubscriptionPacket/SubscriptionCommand.cs
@@ -14,49 +14,99 @@ namespace Sttp.WireProtocol.SubscriptionPacket
         Invalid = 0x00,
 
         /// <summary>
-        /// Stops receiving data for an active subscription
+        /// Indicates that a request for data is being populated.
+        /// Payload: 
+        /// None
         /// </summary>
-        StopSubscription,
+        BeginRequest,
 
         /// <summary>
-        /// Resets all options for a pending subscription request.
+        /// Indicates that the request for data has been completed and should be executed upon.
+        /// Payload: 
+        /// None
         /// </summary>
-        Reset,
+        CompleteRequest,
 
         /// <summary>
-        /// Creates a universe subscription
+        /// Determines if this subscription is an augmentation of an existing subscription. Default is false.
+        /// 
+        /// Payload:
+        /// bool isAugmentationSubscription
         /// </summary>
-        SubscribeToAll,
+        IsAugmentedSubscription,
 
         /// <summary>
-        /// Specifies a list of IDs to subscribe to.
+        /// Sets the down-sampling mode for this subscription to be a certain number of samples per day.
+        /// This is valid for all data points defined after this point.
+        /// 
+        /// long desiredSamplesPerDay
         /// </summary>
-        SubsribeToList,
+        SetDownSamplingPerDay,
 
         /// <summary>
-        /// Removes items from an existing list.
+        /// Sets the down-sampling mode for this subscription to be a value every 'X' number of seconds.
+        /// This is valid for all data points defined after this point.
+        /// 
+        /// long secondsPerSample,
         /// </summary>
-        UnsubscribeFromList,
-        
-        /// <summary>
-        /// Sets the down-sampling mode for this subscription.
-        /// </summary>
-        SetDownSampling,
+        SetDownSamplingSamplesPerSecond,
 
         /// <summary>
-        /// Sets the start time for a subscription for historical data.
+        /// Sets the priority 
+        /// This is valid for all data points defined after this point.
+        /// 
+        /// PriorityFlags flags.
+        /// </summary>
+        SetPriority,
+
+        /// <summary>
+        /// Indicates that all of the following data points should be added the subscription.
+        /// </summary>
+        SubscribeToTheFollowing,
+
+        /// <summary>
+        /// Indicates that all of the following data points should be removed from the subscription.
+        /// </summary>
+        UnsubscribeToTheFollowing,
+
+        /// <summary>
+        /// Identifies all data points.
+        /// </summary>
+        AllDataPoints,
+
+        /// <summary>
+        /// Identifies all Data Points listed in a table
+        /// 
+        /// Payload:
+        /// TableIndex
+        /// </summary>
+        TableDataPoints,
+
+        /// <summary>
+        /// Specifies a DataPoint by ID
+        /// 
+        /// Payload: 
+        /// int DataPointID
+        /// </summary>
+        DatapointByID,
+
+        /// <summary>
+        /// Sets the start time for a subscription for historical data. Default is null.
+        /// 
+        /// Payload: 
+        /// bool HasValue
+        /// SttpTimestamp startTime
         /// </summary>
         SetStartTime,
 
         /// <summary>
-        /// Sets the stop time for a subscription for historical data.
+        /// Sets the stop time for a subscription for historical data. Default is null.
+        /// 
+        /// Payload:
+        /// bool HasValue
+        /// SttpTimestamp stopTime
         /// </summary>
         SetStopTime,
-
-        /// <summary>
-        /// Begins the stream for the existing built request.
-        /// </summary>
-        BeginStream,
         
     }
 }
