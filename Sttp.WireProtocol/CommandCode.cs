8f7b431f7a6699faa6852475f73f16747ba30f32
Steven Chisholm <steven@chizware.com>
2017-10-30 10:42:37 -0500

    Pushed the metadata commands down to the CommandCode level. Reduced the commands from 6 to 2

--- Sttp.WireProtocol/CommandCode.cs
+++ Sttp.WireProtocol/CommandCode.cs
@@ -35,8 +35,6 @@ namespace Sttp.WireProtocol
         /// Fragmented packets can have a compression=none.
         /// 
         /// Layout:
-        /// byte CommandCode.BeginFragment 
-        /// short PacketLength
         /// int TotalFragmentSize     - The size of all fragments.
         /// int TotalRawSize          - The uncompressed data size.
         /// byte CommandCode          - The Command of the data that is encapsulated.
@@ -50,8 +48,6 @@ namespace Sttp.WireProtocol
         /// Specifies the next fragment of data. When Offset + Length of data == TotalFragmentSize, the fragment is completed.
         /// 
         /// Layout:
-        /// byte CommandCode.NextFragment 
-        /// short PacketLength
         /// (Implied) Length of fragment
         /// byte[] Fragment               
         /// </summary>
@@ -61,8 +57,6 @@ namespace Sttp.WireProtocol
         /// Indicates a packet that is compressed but not fragmented. This will decrease the overhead 
         /// 
         /// Layout:
-        /// byte CommandCode.CompressedPacket 
-        /// short PacketLength
         /// int TotalRawSize          - The uncompressed data size.
         /// byte CommandCode,         - The Command of the data that is encapsulated.
         /// byte CompressionMode      - The algorithm that is used to compress the data.
@@ -70,6 +64,59 @@ namespace Sttp.WireProtocol
         /// byte[] CompressedData
         /// </summary>
         CompressedPacket,
+
+        /// <summary>
+        /// Requests the schema for the metadata. Since all I/O for metadata requests occurs 
+        /// with runtime IDs, the response for this packet will provide all table and column
+        /// for the database.
+        /// 
+        /// Layout:
+        /// SubCommands: GetDatabaseSchema     - Will return all tables with all columns.
+        /// SubCommands: GetDatabaseVersion    - Will return the version of the current database.
+        /// 
+        /// Response:
+        /// IF (GetDatabaseSchema)
+        /// Subcommands: AddTable           
+        /// Subcommands: AddColumn
+        /// 
+        /// IF (GetDatabaseVerion)
+        /// Subcommands: DatabaseVersion
+        /// </summary>
+        MetadataGetSchema,
+
+        /// <summary>
+        /// Requests queries from the metadata repository.
+        /// 
+        /// Results will always be returned as a set of individual tables. 
+        /// The purpose of the JOIN clause is only as a filter on the raw tables. If the client wants to 
+        /// create a single table of the results, they will have to join them on their side after getting the results. 
+        /// 
+        /// Layout: 
+        /// Subcommands: SELECT             - Specifies all of the table.columns to include in the query.
+        /// Subcommands: JOIN               - Specifies all of the fields that will be joined in the query. 
+        ///                                   All tables in SELECT that are not TableFlags.MappedToDataPoint
+        ///                                   must be joined to a table that is TableFlags.MappedToDataPoint
+        /// Subcommands: WHERE              - Specifies filters to apply to the results
+        /// Subcommands: DatabaseVersion    - Specifies the current database version if syncing is supported by the client
+        ///                                   Note: When syncing, if any columns specified in the WHERE or JOIN are modified, 
+        ///                                   Syncing will be denied and the entire query must be refreshed. This includes any
+        ///                                   change in any row, not just the rows of the query.
+        /// 
+        /// Response: 
+        /// Subcommands: Clear              - This indicates that the metadata synchronization 
+        ///                                   cannot occur and the entire datasource will be flushed.
+        /// Subcommands: DatabaseVersion
+        /// Subcommands: AddTable
+        /// Subcommands: AddColumn
+        /// Subcommands: AddRow
+        /// Subcommands: AddValue
+        /// Subcommands: DeleteRow          - This command will only appear if syncing a data source.
+        /// 
+        /// </summary>
+        MetadataGetData,
+
+
+
 
         // TODO : assign values
         NegotiateSession,
---
+++ Sttp.WireProtocol/MetadataPacket/MetadataRequest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sttp.WireProtocol.MetadataPacket
+{
+    public enum MetadataSubRequest
+    {
+
+    }
+
+    public class MetadataRequest
+    {
+
+    }
+
+    public class MetadataRequestGetDatabaseSchema
+    {
+
+    }
+
+
+    public class MetadataResponse
+    {
+        public MetadataSubRequest Request;
+
+        public List<Tuple<int, string, TableFlags>> Tables;
+
+        public void AddTable(int tableIndex, string tableNames, TableFlags flags)
+        {
+
+        }
+
+        public void AddColumn(int tableIndex, int columnIndex, string columnName, ValueType columnType)
+        {
+
+        }
+
+        public void AddRow(int tableIndex, int rowIndex)
+        {
+
+        }
+
+        public void AddValue(int tableIndex, int columnIndex, int rowIndex, byte[] value)
+        {
+
+        }
+
+        public void DeleteRow(int tableIndex, int rowIndex)
+        {
+            
+        }
+
+        public void DatabaseVersion(Guid majorVersion, long minorVersion)
+        {
+            
+        }
+    }
+
+
+}
