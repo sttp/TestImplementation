99f2ffce3e517c0883abacce575dd76f2630c693
Steven Chisholm <steven@chizware.com>
2018-03-19 13:07:54 -0500

    Modified SttpTime.

--- Sttp.WireProtocol/SttpTime.cs
+++ Sttp.WireProtocol/SttpTime.cs
@@ -7,41 +7,76 @@ namespace Sttp
     /// </summary>
     public struct SttpTime
     {
-        //This is DateTime.MaxValue + 1
-        private const long LeapSecondRange = 3155378976000000000;
-        private const long TicksMask = (1L << 62) - 1;
+        //((DateTime.MaxValue.Ticks / TimeSpan.TicksPerMinute) << 30) + TimeSpan.TicksPerSecond * 61;
+        private const long MaxValue = 5646770628038745215;
+
+        private const int LeapSecondCheckRange = 59 * (int)TimeSpan.TicksPerSecond;
+        private const int LeapSecondLowerRange = 60 * (int)TimeSpan.TicksPerSecond;
+        private const int LeapSecondUpperRange = 61 * (int)TimeSpan.TicksPerSecond;
+        private const int TicksPerMinute = (1 << 30) - 1;
+        private const int MinutesPastEpocShift = 30;
         private readonly long m_ticks;
+
+        public SttpTime(long ticks)
+        {
+            m_ticks = ticks;
+            Validate();
+        }
 
         public SttpTime(DateTime value)
         {
-            m_ticks = (long)value.Ticks;
+            m_ticks = value.Ticks / TimeSpan.TicksPerMinute;
+            m_ticks = (m_ticks << MinutesPastEpocShift) + (value.Ticks - (m_ticks * TimeSpan.TicksPerMinute));
+            Validate();
+        }
+
+        public SttpTime(DateTime value, bool leapSecondInProgress)
+        {
+            m_ticks = value.Ticks / TimeSpan.TicksPerMinute;
+            m_ticks = (m_ticks << MinutesPastEpocShift) + (value.Ticks - (m_ticks * TimeSpan.TicksPerMinute));
+
+            if (leapSecondInProgress)
+            {
+                m_ticks += TimeSpan.TicksPerSecond;
+            }
+            Validate();
         }
 
         public SttpTime(ByteReader rd)
         {
             m_ticks = rd.ReadInt64();
+            Validate();
         }
 
-        private long InternalTicks => (m_ticks & TicksMask);
-
-        public DateTime Ticks
+        private void Validate()
         {
-            get
+            if (m_ticks < 0 || m_ticks > MaxValue || ElapsedTicks > LeapSecondUpperRange)
             {
-                if (LeapsecondInProgress)
-                {
-                    long leapSecondRange = (InternalTicks - LeapSecondRange);
-                    long minutesPastEpoc = leapSecondRange / TimeSpan.TicksPerSecond;
-                    long ticksIn60 = leapSecondRange % TimeSpan.TicksPerSecond;
-                    return new DateTime(minutesPastEpoc * TimeSpan.TicksPerMinute + TimeSpan.TicksPerSecond * 59 + ticksIn60); //A leap second will be wrapped into the 59 second mark.
-                }
-                return new DateTime(InternalTicks);
+                throw new Exception("Encoding Error");
             }
         }
 
-        public bool LeapsecondInProgress => InternalTicks >= LeapSecondRange;
+        private int ElapsedTicks => (int)(m_ticks * TicksPerMinute);
 
-        public DateTime AsDateTime => Ticks;
+        private long ElapsedMinutes => (m_ticks >> MinutesPastEpocShift);
+
+        public long Ticks => m_ticks;
+
+        public DateTime AsDateTime
+        {
+            get
+            {
+                long ticks = (ElapsedMinutes * TimeSpan.TicksPerMinute + ElapsedTicks);
+                if (LeapSecondInProgress)
+                {
+                    ticks -= TimeSpan.TicksPerSecond;
+                }
+                return new DateTime(ticks);
+            }
+        }
+
+        public bool LeapSecondInProgress => ElapsedTicks >= LeapSecondLowerRange;
+
 
         public static SttpTime Parse(string value)
         {
@@ -50,7 +85,7 @@ namespace Sttp
 
         public override string ToString()
         {
-            return Ticks.ToString();
+            return AsDateTime.ToString();
         }
 
         public void Save(ByteWriter wr)
